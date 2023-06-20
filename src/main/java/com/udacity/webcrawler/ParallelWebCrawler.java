5d734fd22e134a29516ee0411a9834cf162ddce8
Binay Shah <binayshah@Binays-MacBook-Pro.local>
2023-06-20 18:37:27 +0545

    initial commit

---
+++ src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
@@ -0,0 +1,133 @@
+package com.udacity.webcrawler;
+
+import com.udacity.webcrawler.json.CrawlResult;
+import com.udacity.webcrawler.parser.PageParser;
+import com.udacity.webcrawler.parser.PageParserFactory;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * A concrete implementation of {@link WebCrawler} that runs multiple threads on a
+ * {@link ForkJoinPool} to fetch and process multiple web pages in parallel.
+ */
+final class ParallelWebCrawler implements WebCrawler {
+  private final Clock clock;
+  private final Duration timeout;
+  private final int popularWordCount;
+  private final ForkJoinPool pool;
+  private final int maxDepth;
+  private final List<Pattern> ignoredUrls;
+  private PageParserFactory parserFactory;
+
+
+  @Inject
+  ParallelWebCrawler(
+      Clock clock,
+      PageParserFactory parserFactory,
+      @Timeout Duration timeout,
+      @PopularWordCount int popularWordCount,
+      @MaxDepth int maxDepth,
+      @IgnoredUrls List<Pattern> ignoredUrls,
+      @TargetParallelism int threadCount) {
+    this.clock = clock;
+    this.parserFactory = parserFactory;
+    this.timeout = timeout;
+    this.popularWordCount = popularWordCount;
+    this.maxDepth = maxDepth;
+    this.ignoredUrls = ignoredUrls;
+    this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
+  }
+
+  @Override
+  public CrawlResult crawl(List<String> startingUrls) {
+
+    Instant deadline = clock.instant().plus(timeout);
+    ConcurrentMap<String, Integer> counts = new ConcurrentHashMap<>();
+    ConcurrentSkipListSet<String> visitedUrls = new ConcurrentSkipListSet<>();
+    for (String url : startingUrls) {
+      WebCrawlerTask task = new WebCrawlerTask(url, deadline, maxDepth, counts, visitedUrls, ignoredUrls);
+      pool.invoke(task);
+    }
+
+    if (counts.isEmpty()) {
+      return new CrawlResult.Builder()
+              .setWordCounts(counts)
+              .setUrlsVisited(visitedUrls.size())
+              .build();
+    }
+
+    return new CrawlResult.Builder()
+            .setWordCounts(WordCounts.sort(counts, popularWordCount))
+            .setUrlsVisited(visitedUrls.size())
+            .build();
+  }
+
+  @Override
+  public int getMaxParallelism() {
+    return Runtime.getRuntime().availableProcessors();
+  }
+
+   private class WebCrawlerTask extends RecursiveTask {
+
+    private Instant deadline;
+    private ConcurrentMap<String, Integer> counts;
+    private ConcurrentSkipListSet<String> visitedUrls;
+    private String url;
+    private int maxDepth;
+    private final List<Pattern> ignoredUrls;
+
+
+
+
+    public WebCrawlerTask(String url, Instant deadline, int maxDepth,
+                          ConcurrentMap<String, Integer> counts,
+                          ConcurrentSkipListSet<String> visitedUrls, List<Pattern> ignoredUrls) {
+      this.url = url;
+      this.maxDepth = maxDepth;
+      this.deadline = deadline;
+      this.counts = counts;
+      this.visitedUrls = visitedUrls;
+      this.ignoredUrls = ignoredUrls;
+    }
+
+    @Override
+    protected Map<String, Integer> compute() {
+      if (maxDepth == 0 || clock.instant().isAfter(deadline)) {
+        return Collections.emptyMap();
+      }
+      for (Pattern pattern : ignoredUrls) {
+        if (pattern.matcher(url).matches()) {
+          return Collections.emptyMap();
+        }
+      }
+      if (visitedUrls.contains(url)) {
+        return Collections.emptyMap();
+      }
+      visitedUrls.add(url);
+      PageParser.Result result = parserFactory.get(url).parse();
+      for (Map.Entry<String, Integer> e : result.getWordCounts().entrySet()) {
+        if (counts.containsKey(e.getKey())) {
+          //counts.compute(e.getKey(), (k, v) -> (v == null) ? 1 : v + 1);
+          counts.put(e.getKey(), e.getValue() + counts.get(e.getKey()));
+        } else {
+          counts.put(e.getKey(), e.getValue());
+        }
+      }
+
+      List<WebCrawlerTask> subtasks =
+              result.getLinks().stream().map(link -> new WebCrawlerTask(link, deadline, maxDepth-1, counts, visitedUrls, ignoredUrls))
+                      .collect(Collectors.toList());
+      invokeAll(subtasks);
+
+      return counts;
+    }
+  }
+}
